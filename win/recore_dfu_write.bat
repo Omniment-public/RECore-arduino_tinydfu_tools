3f22b4b058877b021cd5bd7be3b69a9d2dfb939e
r-tamura-omniment <rui-tamura@omniment.co.jp>
2021-01-10 02:14:22 +0900

    呼び出しツール名修正

--- win/recore_dfu_write.bat
+++ win/recore_dfu_write.bat
@@ -7,4 +7,4 @@ set ADDRESS=0x8000000
 rem echo %PORT% %FILEPATH% %ADDRESS%
 rem %adfu_path%tiny_dfu_writer.exe %PORT% %FILEPATH% %ADDRESS%
 
-%adfu_path%tiny_dfu.exe %PORT% %FILEPATH% %ADDRESS%
+%adfu_path%tiny_dfu_writer.exe %PORT% %FILEPATH% %ADDRESS%
